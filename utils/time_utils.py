ca3ca9291304dddcee89f1a92860cdb36b85429b
Julián Arce <juarce@itba.edu.ar>
2023-10-01 02:59:06 -0300

    fix: time_utils message format

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -9,7 +9,7 @@ def log_time(func):
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         total_time = end_time - start_time
-        print(f'{func.__name__} execution time:{total_time:.4f} seconds')
+        print(f'{func.__name__.capitalize()} time: {total_time:.4f}s')
         return result
 
     return timeit_wrapper
